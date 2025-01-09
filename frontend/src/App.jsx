54d10fdd6b4db80d5d32461ed6c78f2e588d80d0
tashad-tk <tashadkhanbist@gmail.com>
2025-01-09 19:33:09 +0530

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { Dashboard } from "./pages/Dashboard";
+import { SendMoney } from "./pages/SendMoney";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
